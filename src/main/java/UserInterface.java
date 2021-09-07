4152f659b4614c990168e2fc6a826f53cb2bb820
gkaterinchik <62135154+gkaterinchik@users.noreply.github.com>
2021-09-07 16:30:15 +0300

    HW2

---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,21 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UserInterface {
+    /*public static void hello(AnnotationConfigApplicationContext context){
+        System.out.println("получить список доступных товаров -  /getProductList");
+        System.out.println("добавить товар в корзину /addToCart id");
+        System.out.println("удалить товар из корзины /delete productID");
+        System.out.println("посмотреть содержимое корзины showCart");
+    }*/
+   /* public static void startControl(AnnotationConfigApplicationContext context) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String command = reader.readLine();
+    }
+    */
+}
